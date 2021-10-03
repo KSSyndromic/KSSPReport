7b196b482a5491c49cf8b7a1a2112c45986723fb
hmenag1 <4911460+hmenag1@users.noreply.github.com>
2021-10-03 01:56:48 -0500

    Successfully added get_req_nulls_BZ.R to the package

--- R/get_req_nulls_BZ.R
+++ R/get_req_nulls_BZ.R
@@ -1,17 +1,20 @@
-#' Getting Nulls Summaries for Required Fields
+
+#' Get Null Summaries for Required Fields
+#' 
+#' @description This will generate percent and counts of null message, 
+#'     by facility. All are calculated on a once-per-visit basis, except for:
+#'     Treating_Facility_ID, Trigger_Event, Message_Control_ID, Processing_ID, Version_ID, Message_Profile_ID, 
+#'     Event_Type_Code, Recorded_Date_Time, Message_Date_Time, First_Patient_ID, Medical_Record_Number. 
+#'     These fields are required every single message, so null counts are based on messages. 
+#'     The rest are required only once per-visit, so the whole visit is considered null *only* 
+#'     if *every* record for that field from that visit was null.
+#' 
+#' @details The distinctions between optional vs. invalid and per visit vs. every message were made based off 
+#'     of our interpretation of the PHIN guide as well as the needs of the Kansas Department of Health and 
+#'     Environment Syndromic Surveillance.
 #'
-#' @description
-#' This will generate percent and counts of null message, by facility. All are calculated on a once-per-visit basis, except for:
-#' Treating_Facility_ID, Trigger_Event, Message_Control_ID, Processing_ID, Version_ID, Message_Profile_ID, Event_Type_Code,
-#' Recorded_Date_Time, Message_Date_Time, First_Patient_ID, Medical_Record_Number. These fields are required every single message, so
-#' null counts are based on messages. The rest are required only once per-visit, so the whole visit is considered null *only*
-#' if *every* record for that field from that visit was null.
+#' @param data The raw data from BioSense on which you will do the null checks. 
 #'
-#' @details
-#' The distinctions between optional vs. invalid and per visit vs. every message were made based off of our interpretation of the
-#' PHIN guide as well as the needs of the Kansas Department of Health and Environment Syndromic Surveillance.
-#'
-#' @param data The raw data from BioSense on which you will do the null checks.
 #' @return A summary data frame that lists counts and percentages for null fields, summarized at facility-level.
 #' @import dplyr
 #' @import tidyr
