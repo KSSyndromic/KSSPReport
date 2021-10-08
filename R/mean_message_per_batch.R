2e6e1c9b4941d29649f7356085a9394f4aaa5801
hmenag1 <4911460+hmenag1@users.noreply.github.com>
2021-10-07 22:31:15 -0500

    added mean_message_per_batch function

---
+++ R/mean_message_per_batch.R
@@ -0,0 +1,17 @@
+## compute the average number of messages (with same File_Name) per batch for each Feed_Name
+
+#' compute the average number of messages 
+#'
+#' @param data a data frame
+#'
+#' @return a data frame
+mean_message_per_batch<-function(data){
+  Message_Per_Batch=data%>%
+    group_by(Feed_Name,File_Name)%>%
+    summarise(count=n())%>%
+    ungroup()%>%
+    group_by(Feed_Name)%>%
+    summarise(Batch_Mean=round(mean(count),2))
+  
+  return(Message_Per_Batch)
+}
