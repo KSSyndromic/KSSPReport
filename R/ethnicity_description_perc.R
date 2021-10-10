8d690ed73f103e2c13ed38c9321546548b7ea4ce
hmenag1 <4911460+hmenag1@users.noreply.github.com>
2021-10-10 00:44:12 -0500

    adding ethnicity_description_perc

---
+++ R/ethnicity_description_perc.R
@@ -0,0 +1,21 @@
+##Frequency of occurrence in data - what is actually occurring in the data and
+##how often each distinct entry is occurring and percentage of all visits each entry
+##accounts for. 
+## If the input is empty, say "Missing"
+## for variable Race_Description
+
+## Ethnicity_Description
+ethnicity_description_perc<-function(data){
+  Ethnicity_Description=data%>%
+    select(C_BioSense_ID,Ethnicity_Description)%>%
+    mutate(Ethnicity_Description=toupper(Ethnicity_Description),
+           Ethnicity_Description=ifelse(is.na(Ethnicity_Description),"Missing",Ethnicity_Description))%>%
+    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    count(Ethnicity_Description)%>%
+    transmute(Ethnicity_Description,count=n,percentage=round(100*n/sum(n),2))
+  
+  return(
+    Ethnicity_Description
+  )
+  
+}
