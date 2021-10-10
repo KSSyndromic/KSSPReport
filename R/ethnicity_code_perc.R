3e73c7ca9e2adc3ca66d634ced789588aa403ea6
hmenag1 <4911460+hmenag1@users.noreply.github.com>
2021-10-10 00:49:34 -0500

    adding ethnicity_code_perc

---
+++ R/ethnicity_code_perc.R
@@ -0,0 +1,18 @@
+##Frequency of occurrence in data - what is actually occurring in the data and
+##how often each distinct entry is occurring and percentage of all visits each entry
+##accounts for. 
+## If the input is empty, say "Missing"
+## Ethnicity_Code
+ethnicity_code_perc<-function(data){
+  Ethnicity_Code=data%>%
+    select(C_BioSense_ID,Ethnicity_Code)%>%
+    mutate(Ethnicity_Code=ifelse(is.na(Ethnicity_Code),"Missing",Ethnicity_Code))%>%
+    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    count(Ethnicity_Code)%>%
+    transmute(Ethnicity_Code,count=n,percentage=round(100*n/sum(n),2))
+  
+  return(
+    Ethnicity_Code
+  )
+  
+}
