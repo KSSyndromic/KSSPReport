63755ee39de8f418210cfeb471a54e295961de22
hmenag1 <4911460+hmenag1@users.noreply.github.com>
2021-10-10 22:50:01 -0500

    adding city_perc

---
+++ R/city_perc.R
@@ -0,0 +1,18 @@
+##Frequency of occurrence in data - what is actually occurring in the data and
+##how often each distinct entry is occurring and percentage of all visits each entry
+##accounts for. 
+## If the input is empty, say "Missing"
+##city
+
+city_perc<-function(data){
+  City=data%>%
+    select(C_BioSense_ID, Patient_City)%>%
+     mutate(Patient_City=toupper(Patient_City),
+            Patient_City=ifelse(is.na(Patient_City),"Missing",Patient_City))%>%
+    distinct(C_BioSense_ID,.keep_all=TRUE)%>%
+    count(Patient_City)%>%
+    transmute(Patient_City,count=n,percentage=round(100*n/sum(n),2))
+  return(
+    City
+  )
+}
